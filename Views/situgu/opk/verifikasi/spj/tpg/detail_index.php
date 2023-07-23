5b40d5434413de13687e1ca654af4a161c369451
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-07-23 23:50:06 +0700

    add verifikasi opk

--- Views/situgu/opk/verifikasi/spj/tpg/detail_index.php
+++ Views/situgu/opk/verifikasi/spj/tpg/detail_index.php
@@ -10,11 +10,11 @@
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
                     <h4 class="mb-sm-0 font-size-18">VERIFIKASI USULAN <?= $kode_usulan ?></h4>
 
-                    <!-- <div class="page-title-right">
+                    <div class="page-title-right">
                         <ol class="breadcrumb m-0">
-                            <li class="breadcrumb-item"><a href="javascript:actionSyncAll(this);" class="btn btn-primary btn-rounded waves-effect waves-light">Syncrone Semua Data PTK</a></li>
+                            <li class="breadcrumb-item"><a href="./data" class="btn btn-primary btn-rounded waves-effect waves-light">Kembali</a></li>
                         </ol>
-                    </div> -->
+                    </div>
 
                 </div>
             </div>
