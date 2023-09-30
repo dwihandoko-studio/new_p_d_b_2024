f5c246df8b063775857c82aba985fb9eef255d3e
dwihandoko-studio <dwi.handoko0511@gmail.com>
2023-09-30 17:08:14 +0700

    add ref tahun tw

--- Views/situgu/su/masterdata/reftw/index.php
+++ Views/situgu/su/masterdata/reftw/index.php
@@ -10,11 +10,11 @@
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
                     <h4 class="mb-sm-0 font-size-18">REFERENSI TAHUN TW</h4>
 
-                    <div class="page-title-right">
+                    <!-- <div class="page-title-right">
                         <ol class="breadcrumb m-0">
                             <li class="breadcrumb-item"><a href="javascript: actionAdd(this);">Tambah</a></li>
                         </ol>
-                    </div>
+                    </div> -->
 
                 </div>
             </div>
@@ -25,9 +25,14 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <div class="row">
-                            <div class="col-6">
-                                <h4 class="card-title">Data Referensi Tahun TW</h4>
+                        <div class="card-header">
+                            <div class="row">
+                                <div class="col-6">
+                                    <h4 class="card-title">Data Referensi Tahun TW</h4>
+                                </div>
+                                <div class="col-6 text-end">
+                                    <a href="javascript:actionAdd(this);" class="btn btn-primary btn-rounded waves-effect waves-light" style="min-width: 6rem;"><i class="bx bxs-plus-circle font-size-16 align-middle me-2"></i> Tambah </a>
+                                </div>
                             </div>
                         </div>
                     </div>
