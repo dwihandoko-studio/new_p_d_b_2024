72f98506d5c01ae687a70d47b26189a01c718a9f
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-03-25 09:26:30 +0700

    add update app sigaji

---
+++ Controllers/Sigaji/Maintenance.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controllers\Sigaji;
+
+use App\Controllers\BaseController;
+use App\Libraries\Profilelib;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use App\Libraries\Mtlib;
+
+class Maintenance extends BaseController
+{
+    private $_db;
+
+    function __construct()
+    {
+        helper(['text', 'file', 'form', 'array', 'imageurl', 'web', 'filesystem']);
+        $this->_db      = \Config\Database::connect();
+    }
+    public function index()
+    {
+        $mtLib = new Mtlib();
+        if (!$mtLib->get(3, '_tb_maintenance_gaji')) {
+            return redirect()->to(base_url('sigaji'));
+        }
+
+        $data['title'] = "MAINTENANCE";
+        $template = $this->_db->table('_tb_maintenance')->where(['id' => 1, 'template_active' => 1])->get()->getRowObject();
+        if ($template) {
+            if ($template->template !== null) {
+                $activeTemplate = $template->template;
+            } else {
+                $activeTemplate = 'index';
+            }
+        } else {
+            $activeTemplate = 'index';
+        }
+        return view('situgu/mt/' . $activeTemplate, $data);
+    }
+}
