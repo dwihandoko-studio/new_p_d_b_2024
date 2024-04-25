e7be12e0d0162fe085a6392339b77d37f32ea068
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-04-26 00:41:40 +0700

    add user bank on sigaji

--- Views/sigaji/bank/tagihan/antrian/content_add.php
+++ Views/sigaji/bank/tagihan/antrian/content_add.php
@@ -29,12 +29,13 @@
                         <input class="form-check-input" type="checkbox" id="formCheck1">
                     </td>
                     <td>
-                        <select class="form-control filter-pegawai" id="_filter_pegawai" name="_filter_pegawai" required>
+                        <select class="form-control filter-pegawai" id="_filter_pegawai_1" name="_filter_pegawai[]" data-id="1" onchange="changePegawai(this)" required>
                             <option value="">&nbsp;</option>
                         </select>
                         <script>
-                            $('#_filter_pegawai').select2({
+                            $('#_filter_pegawai_1').select2({
                                 dropdownParent: ".data-contens",
+                                allowClear: true,
                                 ajax: {
                                     url: "./getPegawai",
                                     type: 'POST',
@@ -97,18 +98,21 @@
                             }
 
                             function formatRepoSelection(repo) {
+                                $(repo.element).attr('data-custom-nip', repo.nip);
+                                $(repo.element).attr('data-custom-instansi', repo.nama_instansi);
+                                $(repo.element).attr('data-custom-kecamatan', repo.nama_kecamatan);
                                 return repo.nama || repo.text;
                             }
                         </script>
                     </td>
                     <td>
-                        <input class="form-control" type="text" value="Nip" id="example-text-input">
+                        <input class="form-control" type="text" value="" id="nip_1" name="nip[]" readonly>
                     </td>
                     <td>
-                        <input class="form-control" type="text" value="instansi" id="example-text-input">
+                        <input class="form-control" type="text" value="" id="instansi_1" name="instansi[]" readonly>
                     </td>
                     <td>
-                        <input class="form-control" type="text" value="kecamatan" id="example-text-input">
+                        <input class="form-control" type="text" value="" id="kecamatan_1" name="kecamatan[]" readonly>
                     </td>
                     <td>
                         <input class="form-control" type="number" value="1" id="example-text-input">
@@ -139,6 +143,7 @@
                     <script>
                         $('#_filter_pegawai').select2({
                             dropdownParent: ".data-contens",
+                            allowClear: true,
                             ajax: {
                                 url: "./getPegawai",
                                 type: 'POST',
@@ -233,3 +238,57 @@
         <?php } ?>
     </tbody>
 </table>
+
+<script>
+    function changePegawai(event) {
+        const getId = $(event).data('id');
+        const getNip = $('#_filter_pegawai_' + getId).find(':selected').data('custom-nip');
+        const getInstansi = $('#_filter_pegawai_' + getId).find(':selected').data('custom-instansi');
+        const getKecamatan = $('#_filter_pegawai_' + getId).find(':selected').data('custom-kecamatan');
+
+        $('#nip_' + getId).val(getNip);
+        // $('#nip_' + getId).val(getNip);
+        // // $(event).removeAttr('style');
+        // // $('.' + color).html('');
+
+        // if (event.value !== "") {
+        //     $.ajax({
+        //         url: './getPengguna',
+        //         type: 'POST',
+        //         data: {
+        //             id: event.value,
+        //         },
+        //         dataType: 'JSON',
+        //         beforeSend: function() {
+        //             $('div._pengguna-block').block({
+        //                 message: '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span>'
+        //             });
+        //         },
+        //         success: function(msg) {
+        //             $('div._pengguna-block').unblock();
+        //             if (msg.status == 200) {
+        //                 let html = "";
+        //                 html += '<option value="">--Pilih--</option>';
+        //                 if (msg.data.length > 0) {
+        //                     for (let step = 0; step < msg.data.length; step++) {
+        //                         html += '<option value="';
+        //                         html += msg.data[step].id;
+        //                         html += '">';
+        //                         html += msg.data[step].fullname;
+        //                         html += ' (';
+        //                         html += msg.data[step].email;
+        //                         html += ')</option>';
+        //                     }
+
+        //                 }
+
+        //                 $('.pengguna').html(html);
+        //             }
+        //         },
+        //         error: function(data) {
+        //             $('div._pengguna-block').unblock();
+        //         }
+        //     })
+        // }
+    }
+</script>
