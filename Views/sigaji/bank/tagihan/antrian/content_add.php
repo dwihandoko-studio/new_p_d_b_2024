f909a143338602dc9515da9285bf7e06b4d5b67a
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-04-26 08:11:44 +0700

    add user bank on sigaji

--- Views/sigaji/bank/tagihan/antrian/content_add.php
+++ Views/sigaji/bank/tagihan/antrian/content_add.php
@@ -266,7 +266,7 @@
                             <input class="form-control" type="number" value="" id="angsuran_ke_${rowBody}" name="angsuran_ke[]" required>
                         </td>
                         <td>
-                            <button type="button" onclick="aksiTambah(this)" class="btn btn-primary btn-rounded waves-effect waves-light">+</button>
+                            <button type="button" class="btn btn-danger btn-rounded waves-effect waves-light btnhapusform">x</button>
                         </td>
                     </tr>`;
 
