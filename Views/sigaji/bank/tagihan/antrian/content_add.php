4524fcc310985bbbd987ab098b477d3ceb12d1a9
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-04-26 14:41:17 +0700

    add user bank on sigaji

--- Views/sigaji/bank/tagihan/antrian/content_add.php
+++ Views/sigaji/bank/tagihan/antrian/content_add.php
@@ -3,11 +3,11 @@
         <tr>
             <th data-orderable="false">#</th>
             <th data-orderable="false" width="20%">Nama</th>
-            <th data-orderable="false" width="13%">NIP</th>
+            <th data-orderable="false" width="14%">NIP</th>
             <th data-orderable="false">Instansi</th>
             <th data-orderable="false">Kecamatan</th>
             <th data-orderable="false" width="11%">Besar Pinjaman</th>
-            <th data-orderable="false" width="9%">Jumlah Tagihan</th>
+            <th data-orderable="false" width="10%">Jumlah Tagihan</th>
             <th data-orderable="false" width="5%">Jml Bulan<br>Angs</th>
             <th data-orderable="false" width="5%">Angs Ke</th>
             <th data-orderable="false"> </th>
