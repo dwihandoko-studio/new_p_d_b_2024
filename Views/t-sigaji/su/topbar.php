512b2b962fa174d3dabb728d375ff4d6b6838300
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-04-29 15:16:05 +0700

    add user bank on sigaji

--- Views/t-sigaji/su/topbar.php
+++ Views/t-sigaji/su/topbar.php
@@ -45,6 +45,8 @@
                                 <a href="<?= base_url('sigaji/su/upload/pegawai') ?>" class="dropdown-item <?= ($uri->getSegment(2) == "su" && $uri->getSegment(3) == "upload" && $uri->getSegment(4) == "pegawai") ? ' active-menu-href' : '' ?>" key="t-upload-pegawai">Gaji SIPD</a>
                                 <a href="<?= base_url('sigaji/su/upload/tagihanbank') ?>" class="dropdown-item <?= ($uri->getSegment(2) == "su" && $uri->getSegment(3) == "upload" && $uri->getSegment(4) == "tagihanbank") ? ' active-menu-href' : '' ?>" key="t-upload-tagihanbank">Tagihan Bank</a>
                                 <a href="<?= base_url('sigaji/su/upload/meninggal') ?>" class="dropdown-item <?= ($uri->getSegment(2) == "su" && $uri->getSegment(3) == "upload" && $uri->getSegment(4) == "meninggal") ? ' active-menu-href' : '' ?>" key="t-upload-meninggal">Data Meninggal</a>
+                                <a href="<?= base_url('sigaji/su/upload/zakat') ?>" class="dropdown-item <?= ($uri->getSegment(2) == "su" && $uri->getSegment(3) == "upload" && $uri->getSegment(4) == "zakat") ? ' active-menu-href' : '' ?>" key="t-upload-zakat">Zakat</a>
+                                <a href="<?= base_url('sigaji/su/upload/shodaqoh') ?>" class="dropdown-item <?= ($uri->getSegment(2) == "su" && $uri->getSegment(3) == "upload" && $uri->getSegment(4) == "shodaqoh") ? ' active-menu-href' : '' ?>" key="t-upload-shodaqoh">Shodaqoh</a>
                             </div>
                         </li>
                         <li class="nav-item dropdown">
