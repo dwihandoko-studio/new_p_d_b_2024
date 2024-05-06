381f2ea766206a43237fcbf52d318c46738d243b
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-05-07 00:09:13 +0700

    add user bank on sigaji

--- Controllers/Sigaji/Bank/Tagihan/Antrian.php
+++ Controllers/Sigaji/Bank/Tagihan/Antrian.php
@@ -359,8 +359,16 @@ extends BaseController
 
             $jmlData = count($nips);
             if ($jmlData === count($instansis) && $jmlData === count($kecamatans) && $jmlData === count($jumlah_pinjamans) && $jmlData === count($jumlah_tagihans) && $jmlData === count($jumlah_bulan_angsurans) && $jmlData === count($angsuran_kes)) {
-                $uuidLib = new Uuid();
+
                 $id_bank = $this->_helpLib->getIdBank($user->data->id);
+                $deletedAllData = $this->_db->table('tb_tagihan_bank_antrian')->where(['tahun' => $id, 'dari_bank' => $id_bank])->delete();
+                if (!($this->_db->affectedRows() > 0)) {
+                    $response = new \stdClass;
+                    $response->status = 400;
+                    $response->message = "Gagal memvalidasi data.";
+                    return json_encode($response);
+                }
+                $uuidLib = new Uuid();
                 $dataInserts = [];
 
                 for ($i = 0; $i < $jmlData; $i++) {
