09c37f3a28ac4beab73b4e0c8c2785f94cf6f4fd
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-05-15 11:00:14 +0700

    add input bank on sigaji

--- Views/sigaji/bank/tagihan/antrian/content_add_ambil_tagihan.php
+++ Views/sigaji/bank/tagihan/antrian/content_add_ambil_tagihan.php
@@ -213,6 +213,14 @@
     <?php } ?>
 
     $(document).ready(function() {
+
+        let tableDatatables = $("#data-datatables").DataTable({
+            columnDefs: [{
+                orderable: false,
+                targets: [0, 1, 2, 3, 4, 5, 6, 7, 8]
+            }],
+        });
+
         $('#_filter_pegawai_' + rowBody).select2({
             dropdownParent: ".data-contens",
             allowClear: true,
