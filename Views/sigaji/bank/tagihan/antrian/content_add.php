f5b266334c482802b3204a10560276d37946663c
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-05-20 17:48:11 +0700

    add edited form on usulan bank

--- Views/sigaji/bank/tagihan/antrian/content_add.php
+++ Views/sigaji/bank/tagihan/antrian/content_add.php
@@ -244,6 +244,10 @@
                 orderable: false,
                 targets: [0, 1, 2, 3, 4, 5, 6, 7, 8]
             }],
+            aLengthMenu: [
+                [10, 25, 50, 100, 200, -1],
+                [10, 25, 50, 100, 200, "All"]
+            ],
             buttons: ["copy", "excel", "pdf"]
         }).buttons().container().appendTo("#data-datatables_wrapper .col-md-6:eq(0)");
 
