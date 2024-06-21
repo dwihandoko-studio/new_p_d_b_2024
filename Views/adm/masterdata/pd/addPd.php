4492deec7ab0744823262dfec4712fe372dcdcd6
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-06-21 13:34:34 +0700

    update masterdata pd on adm

--- Views/adm/masterdata/pd/addPd.php
+++ Views/adm/masterdata/pd/addPd.php
@@ -117,7 +117,7 @@
                         <?php if (isset($dusuns)) { ?>
                             <?php if (count($dusuns) > 0) { ?>
                                 <?php foreach ($dusuns as $key => $value) { ?>
-                                    <option value="<?= $value->id ?>" <?= ($value->id === $data->dusun) ? ' selected' : "" ?>><?= $value->nama ?></option>
+                                    <option value="<?= $value->id ?>"><?= $value->nama ?></option>
                                 <?php } ?>
                             <?php } ?>
                         <?php } ?>
