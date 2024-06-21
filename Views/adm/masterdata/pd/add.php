7fb3993b329e46759daa6505544677809494f748
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-06-22 03:48:16 +0700

    update masterdata pd on adm

--- Views/adm/masterdata/pd/add.php
+++ Views/adm/masterdata/pd/add.php
@@ -43,7 +43,7 @@
     </div>
     <div class="modal-footer">
         <button type="button" class="btn btn-danger light" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">AMBIL DATA</button>
+        <button type="submit" class="btn btn-primary">CEK DATA</button>
     </div>
 </form>
 <script>
