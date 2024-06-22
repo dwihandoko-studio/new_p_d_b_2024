ee204e7225ee89e26b8e40cc4bbe12496975b2f9
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-06-22 21:19:48 +0700

    update masterdata pd on adm

--- Views/dashboard/zonasi_wilayah.php
+++ Views/dashboard/zonasi_wilayah.php
@@ -91,72 +91,7 @@
                 </div>
             </div>
 
-            <div class="col-xl-3 col-xxl-4">
-                <div class="card">
-                    <div class="card-header border-0 pb-0">
-                        <div>
-                            <h4 class="fs-20 font-w700 mb-2">Dinas Pendidikan dan Kebudayaan</h4>
-                            <span class="fs-14">Kabupaten Lampung Tengah</span>
-                        </div>
-                    </div>
-                    <div class="card-body pt-3">
-                        <p class="description">Jl. H Muchtar RT. 03 /RW. 01, Komplek Perkantoran Gunung Sugih No. 1, Lampung Tengah, Gn. Sugih, Kec. Gn. Sugih, Metro Lampung, Lampung 34161</p>
-                        <ul class="specifics-list">
-                            <li>
-                                <span class="bg-blue"></span>
-                                <div>
-                                    <h4>CS: SMP</h4>
-                                    <span>0853-2791-9291</span>
-                                </div>
-                            </li>
-                            <li>
-                                <span class="bg-red"></span>
-                                <div>
-                                    <h4>CS: SD</h4>
-                                    <span>0852-6902-2481</span>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="col-xl-9 col-xxl-8">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-xl-4">
-                                <div class="mail-list rounded">
-                                    <div class="intro-title d-flex justify-content-between my-0">
-                                        <h5>Halaman</h5>
-                                    </div>
-                                    <p style="margin-bottom: 0px;"><a href="<?= base_url('home/data') ?>">Home</a></p>
-                                    <p style="margin-bottom: 0px;"><a href="<?= base_url('home/jadwal') ?>">Jadwal Pendaftaran</a></p>
-                                    <p style="margin-bottom: 0px;"><a href="<?= base_url('home/kuota') ?>">Kuota Sekolah</a></p>
-                                    <p style="margin-bottom: 0px;"><a href="<?= base_url('home/zonasi_wilayah') ?>">Zonasi Wilayah</a></p>
-                                    <p style="margin-bottom: 0px;"><a href="<?= base_url('home/statistik') ?>">Statistik</a></p>
-                                </div>
-                            </div>
-                            <div class="col-xl-4">
-                                <div class="mail-list rounded">
-                                    <div class="intro-title d-flex justify-content-between my-0">
-                                        <h5>Tautan</h5>
-                                    </div>
-                                    <p><a href="https://disdikbud.lampungtengahkab.go.id">Website Dinas Pendidikan dan Kebudayaan Kab. Lampung Tengah</a></p>
-                                </div>
-                            </div>
-                            <div class="col-xl-4">
-                                <div class="mail-list rounded">
-                                    <div class="intro-title d-flex justify-content-between my-0">
-                                        <h5>Pengaduan</h5>
-                                    </div>
-                                    <p>Punya Kendala PPDB </p>
-                                    <button class="btn btn-primary ms-5">BUAT PENGADUAN</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <?= $this->include('t-dashboard/bottom'); ?>
 
         </div>
     </div>
