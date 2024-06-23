f8d64028bcf0dc2a1c68f830c873aeda8c027637
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-06-23 16:12:07 +0700

    update masterdata pd on adm

---
+++ Views/pd/informasi/panduan/index.php
@@ -0,0 +1,120 @@
+<?= $this->extend('t-ppdb/pd/index'); ?>
+
+<?= $this->section('content'); ?>
+
+<div class="content-body">
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="card">
+                    <div class="card-header d-block d-sm-flex border-0 transactions-tab">
+                        <div class="me-3">
+                            <h4 class="card-title mb-2">Panduan Aplikasi PPDB</h4>
+                            <span class="fs-12">Informasi panduan Aplikasi PPDB Kab. Lampung Tengah Tahun 2024/2025</span>
+                        </div>
+                        <div class="card-tabs mt-3 mt-sm-0">
+                        </div>
+                    </div>
+                    <div class="card-body tab-content p-0">
+                        <style>
+                            .image-container-panduan {
+                                display: flex;
+                                justify-content: center;
+                                align-items: center;
+                                width: 100%;
+                                height: 100vh;
+                                /* 100% viewport height */
+                            }
+
+                            #current-image-panduan {
+                                width: 50%;
+                                /* Adjust as needed */
+                                max-height: 80%;
+                                /* Adjust as needed */
+                            }
+
+                            .prev-btn-panduan,
+                            .next-btn-panduan {
+                                position: absolute;
+                                top: 50%;
+                                transform: translateY(-50%);
+                                padding: 10px;
+                                border: none;
+                                cursor: pointer;
+                            }
+
+                            .prev-btn-panduan {
+                                left: 10px;
+                            }
+
+                            .next-btn-panduan {
+                                right: 10px;
+                            }
+                        </style>
+                        <div class="image-container-panduan">
+                            <img src="<?= base_url() ?>/uploads/panduan/1.svg" alt="Image 1" id="current-image-panduan">
+                            <button class="prev-btn-panduan">Previous</button>
+                            <button class="next-btn-panduan">Next</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="content-firsloginModal" class="modal fade content-firsloginModal" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content ">
+            <div class="modal-header">
+                <h5 class="modal-title" id="content-firsloginModalLabel">Modal title</h5>
+                </button>
+            </div>
+            <div class="content-firsloginBodyModal">
+            </div>
+        </div>
+    </div>
+</div>
+<?= $this->endSection(); ?>
+
+<?= $this->section('scriptBottom'); ?>
+
+<script>
+    const currentImagePanduan = document.getElementById('current-image-panduan');
+    const prevBtnPanduan = document.querySelector('.prev-btn-panduan');
+    const nextBtnPanduan = document.querySelector('.next-btn-panduan');
+
+    let imageIndexPanduan = 0;
+    const imagesPanduan = ['<?= base_url() ?>/uploads/panduan/1.svg', '<?= base_url() ?>/uploads/panduan/2.svg', '<?= base_url() ?>/uploads/panduan/3.svg', '<?= base_url() ?>/uploads/panduan/4.svg', '<?= base_url() ?>/uploads/panduan/5.svg', '<?= base_url() ?>/uploads/panduan/6.svg', '<?= base_url() ?>/uploads/panduan/7.svg', '<?= base_url() ?>/uploads/panduan/8.svg', '<?= base_url() ?>/uploads/panduan/9.svg', '<?= base_url() ?>/uploads/panduan/10.svg']; // Add more image paths here
+
+    function showImagePanduan(index) {
+        currentImagePanduan.src = imagesPanduan[index];
+    }
+
+    showImagePanduan(imageIndexPanduan);
+
+    prevBtnPanduan.addEventListener('click', () => {
+        imageIndexPanduan = imageIndexPanduan === 0 ? imagesPanduan.length - 1 : imageIndexPanduan - 1;
+        showImagePanduan(imageIndexPanduan);
+    });
+
+    nextBtnPanduan.addEventListener('click', () => {
+        imageIndexPanduan = imageIndexPanduan === imagesPanduan.length - 1 ? 0 : imageIndexPanduan + 1;
+        showImagePanduan(imageIndexPanduan);
+    });
+</script>
+<?= $this->endSection(); ?>
+
+<?= $this->section('scriptTop'); ?>
+<style>
+    .lihatPetanya {
+        background-color: #407d4a !important;
+        cursor: pointer;
+        transition: background-color 0.2s ease;
+    }
+
+    .lihatPetanya:hover {
+        background-color: #d653c1 !important;
+    }
+</style>
+<?= $this->endSection(); ?>
