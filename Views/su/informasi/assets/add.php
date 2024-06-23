b7dbb4175c5ab09b9f4942ef7598ff4b3c1f3f2e
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-06-24 00:32:48 +0700

    update dashboard public

--- Views/su/informasi/assets/add.php
+++ Views/su/informasi/assets/add.php
@@ -2,24 +2,9 @@
     <div class="modal-body">
         <div class="row">
             <div class="mb-3 row">
-                <label for="_tujuan" class="col-form-label">Tujuan:</label>
-                <select class="form-control" id="_tujuan" name="_tujuan" width="100%" style="width: 100%;" required>
-                    <option value="">--Pilih--</option>
-                    <option value="all">SEMUA</option>
-                    <option value="3">Admin Sekolah / Panitia Sekolah</option>
-                    <option value="5">Peserta Didik PPDB</option>
-                </select>
-            </div>
-            <div class="mb-3 row">
                 <label class="col-sm-3 col-form-label">Judul :</label>
                 <div class="col-sm-9">
                     <input type="text" class="form-control" id="_judul" name="_judul" value="" placeholder="Judul...." required />
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label class="col-sm-3 col-form-label">Deskripsi :</label>
-                <div class="col-sm-12">
-                    <textarea name="_deskripsi" id="_deskripsi"></textarea>
                 </div>
             </div>
             <div class="col-lg-12">
@@ -27,8 +12,8 @@
                     <div class="col-lg-6">
                         <div class="mt-3">
                             <label for="_file" class="form-label">Upload File : </label>
-                            <input class="form-control" type="file" id="_file" name="_file" onFocus="inputFocus(this);" accept="image/*,application/pdf">
-                            <p class="font-size-11">Format : <code data-toggle="tooltip" data-placement="bottom" title="pdf, jpg, png, jpeg">Files</code> and Maximum File Size <code>2 Mb</code></p>
+                            <input class="form-control" type="file" id="_file" name="_file" onFocus="inputFocus(this);" accept="image/*">
+                            <p class="font-size-11">Format : <code data-toggle="tooltip" data-placement="bottom" title="jpg, png, jpeg">Files</code> and Maximum File Size <code>2 Mb</code></p>
                             <div class="help-block _file" for="_file"></div>
                         </div>
                     </div>
@@ -42,14 +27,6 @@
     </div>
 </form>
 <script>
-    ClassicEditor.create(document.querySelector("#_deskripsi"), {})
-        .then((e) => {
-            window.editor = e;
-        })
-        .catch((e) => {
-            console.error(e.stack);
-        });
-
     document.getElementById('_file').addEventListener('change', loadFile, false);
 
     function loadFile(e) {
@@ -58,7 +35,7 @@
         if (file) {
             const extensionFile = file.name.split('.').pop().toLowerCase();
 
-            var mime_types = ['image/jpg', 'image/jpeg', 'image/png', 'application/pdf'];
+            var mime_types = ['image/jpg', 'image/jpeg', 'image/png'];
 
             if (mime_types.indexOf(file.type) == -1) {
                 e.target.value = "";
@@ -106,7 +83,6 @@
     }
 
     function validateForm(formElement) {
-        const editorData = $('#_deskripsi').val();
         const judul = document.getElementsByName('_judul')[0];
 
         if (judul.value === "" || judul.value === undefined) {
@@ -133,7 +109,7 @@
                 const nama = document.getElementsByName('_judul')[0].value;
                 Swal.fire({
                     title: 'Apakah anda yakin ingin menyimpan data ini?',
-                    text: "Tambah Data Panitia: " + nama,
+                    text: "Tambah Data Gambar: " + nama,
                     showCancelButton: true,
                     icon: 'question',
                     confirmButtonColor: '#3085d6',
@@ -142,16 +118,12 @@
                 }).then((result) => {
                     if (result.value) {
                         const fileName = document.getElementsByName('_file')[0].value;
-                        const tujuan = document.getElementsByName('_tujuan')[0].value;
-                        const deskripsi = $('#_deskripsi').val();
                         const formUpload = new FormData();
                         if (fileName !== "") {
                             const __file = document.getElementsByName('_file')[0].files[0];
                             formUpload.append('_file', __file);
                         }
-                        formUpload.append('_tujuan', tujuan);
                         formUpload.append('_judul', nama);
-                        formUpload.append('_deskripsi', deskripsi);
 
                         $.ajax({
                             xhr: function() {
