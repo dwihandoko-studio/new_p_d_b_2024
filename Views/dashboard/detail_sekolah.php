1f077494e5392c1af733a8fc4cf7d67264553397
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-06-23 18:43:56 +0700

    update masterdata pd on adm

--- Views/dashboard/detail_sekolah.php
+++ Views/dashboard/detail_sekolah.php
@@ -4,19 +4,36 @@
 
 <div class="content-body">
     <div class="container-fluid" style="padding-top: 0.5rem;">
+        <div class="row page-titles">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active"><a href="javascript:void(0)">Profil Sekolah</a></li>
+                <li class="breadcrumb-item"><a href="javascript:void(0)"><?= $sekolah->nama ?></a></li>
+            </ol>
+        </div>
         <div class="row">
-            <div class="col-xl-12">
-                <div class="card">
-                    <div class="card-header d-block d-sm-flex border-0 transactions-tab">
-                        <div class="me-3">
-                            <h4 class="card-title mb-2">Detail Sekolah <?= $sekolah->nama ?></h4>
-                            <span class="fs-12">PPDB Kab. Lampung Tengah Tahun 2024/2025</span>
+            <div class="col-lg-12">
+                <div class="profile card card-body px-3 pt-3 pb-0">
+                    <div class="profile-head">
+                        <div class="photo-content">
+                            <div class="cover-photo rounded"></div>
                         </div>
-                    </div>
-                    <div class="card-body tab-content p-0">
-                        <div class="tab-pane fade active show" id="monthly" role="tabpanel">
-                            <div id="accordion-one" class="accordion style-1">
-
+                        <div class="profile-info">
+                            <div class="profile-photo">
+                                <img style="max-width: 100px; max-height: 100px; width: 100px; height: 100px;" src="<?= $user->image ? base_url('uploads/user') . '/' . $user->image : base_url() . '/assets/images/profile/profile.png' ?>" class="img-fluid rounded-circle" alt="">
+                            </div>
+                            <div class="profile-details">
+                                <div class="profile-name px-3 pt-2">
+                                    <h4 class="text-primary mb-0"><?= $sekolah->nama ?></h4>
+                                    <p><?= $sekolah->npsn ?></p>
+                                </div>
+                                <div class="profile-email px-2 pt-2">
+                                    <h4 class="text-muted mb-0"><?= $sekolah->email ?? '-' ?></h4>
+                                    <p>Email</p>
+                                </div>
+                                <div class="profile-email px-2 pt-2">
+                                    <h4 class="text-muted mb-0"><?= $sekolah->nomor_telepon ?? '-' ?></h4>
+                                    <p>No Telp.</p>
+                                </div>
                             </div>
                         </div>
                     </div>
