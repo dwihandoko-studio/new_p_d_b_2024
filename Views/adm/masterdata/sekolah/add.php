e6b5ba8f47976eda6c24a9243a6c48bbc0d8d52e
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-06-24 18:21:57 +0700

    update dashboard public

--- Views/adm/masterdata/sekolah/add.php
+++ Views/adm/masterdata/sekolah/add.php
@@ -148,7 +148,7 @@
     </div>
     <div class="modal-footer">
         <button type="button" class="btn btn-danger light" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">CEK DATA</button>
+        <button type="submit" class="btn btn-primary">SIMPAN DATA SEKOLAH</button>
     </div>
 </form>
 <script>
