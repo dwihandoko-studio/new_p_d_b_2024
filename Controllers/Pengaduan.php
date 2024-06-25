2e0fb3cfed2dfb80adcb99865333e49161dec5ce
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-06-25 23:14:28 +0700

    update dashboard public

--- Controllers/Pengaduan.php
+++ Controllers/Pengaduan.php
@@ -110,6 +110,44 @@ class Pengaduan extends BaseController
         }
     }
 
+    public function downloadHasilPengaduan()
+    {
+        if ($this->request->isAJAX()) {
+
+            $rules = [
+                'id' => [
+                    'rules' => 'required|trim',
+                    'errors' => [
+                        'required' => 'No tiket tidak boleh kosong. ',
+                    ]
+                ],
+            ];
+
+            if (!$this->validate($rules)) {
+                $response = new \stdClass;
+                $response->status = 400;
+                $response->message = $this->validator->getError('id');
+                return json_encode($response);
+            } else {
+                $tiket = htmlspecialchars($this->request->getVar('id'), true);
+
+                $data = $this->_db->table('dapo_peserta_pengajuan')
+                    ->where('id', $tiket)->get()->getRowObject();
+
+                if (!$data) {
+                    $response = new \stdClass;
+                    $response->status = 400;
+                    $response->message = "Tiket pengaduan tidak ditemukan.";
+                    return json_encode($response);
+                }
+
+                return $this->downloadAkunPrivate($data->peserta_didik_id, $data->nama);
+            }
+        } else {
+            exit('Maaf tidak dapat diproses');
+        }
+    }
+
     public function changeKontak()
     {
         if ($this->request->isAJAX()) {
--- Views/pengaduan/lacak_pengaduan_tiket.php
+++ Views/pengaduan/lacak_pengaduan_tiket.php
@@ -9,7 +9,15 @@
                 <h6 class="mb-0"><?= strtoupper($data->jenis_pengaduan) ?> - Status <strong class="text-primary"><?= getStatusTicketPengaduan($data->status) ?></strong>.</h6>
                 <span><?= $data->nama_pengadu ?></span>
                 <span><?= $data->email_pengadu ?></span>
-                <span><?= $data->nohp_pengadu ?> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<button class="btn btn-xxs btn-light" onclick="changeKontak('<?= $data->no_tiket ?>')">Edit Kontak</button></span>
+                <span><?= $data->nohp_pengadu ?>
+                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<button class="btn btn-xxs btn-light" onclick="changeKontak('<?= $data->no_tiket ?>')">Edit Kontak</button></span>
+                <?php if ((int)$data->status == 3) { ?>
+                    <span>Keterangan Penolakan:</span>
+                    <span><?= $data->keterangan ?></span>
+                <?php } ?>
+                <?php if ((int)$data->status == 2) { ?>
+                    <span><button class="btn btn-xxs btn-primary" onclick="downloadHasilPengaduan('<?= $data->no_tiket ?>')">Download Hasil Pengaduan</button></span>
+                <?php } ?>
             </a>
         </li>
         <!-- <li>
@@ -59,6 +67,54 @@
     function changeKontak(id) {
         $.ajax({
             url: "./changeKontak",
+            type: 'POST',
+            data: {
+                id: id,
+            },
+            dataType: 'JSON',
+            beforeSend: function() {
+                Swal.fire({
+                    title: 'Sedang Loading...',
+                    text: 'Please wait while we process your action.',
+                    allowOutsideClick: false,
+                    allowEscapeKey: false,
+                    didOpen: () => {
+                        Swal.showLoading();
+                    }
+                });
+            },
+            complete: function() {},
+            success: function(response) {
+                if (response.status == 200) {
+                    Swal.close();
+                    $('#content-changeModalLabel').html('EDIT KONTAK TIKET');
+                    $('.content-changeModalBody').html(response.data);
+                    $('.content-changeModal').modal({
+                        backdrop: 'static',
+                        keyboard: false,
+                    });
+                    $('.content-changeModal').modal('show');
+                } else {
+                    Swal.fire(
+                        'Failed!',
+                        response.message,
+                        'warning'
+                    );
+                }
+            },
+            error: function() {
+                Swal.fire(
+                    'PERINGATAN!',
+                    "Server sedang sibuk, silahkan ulangi beberapa saat lagi.",
+                    'warning'
+                );
+            }
+        });
+    }
+
+    function downloadHasilPengaduan(id) {
+        $.ajax({
+            url: "./downloadHasilPengaduan",
             type: 'POST',
             data: {
                 id: id,
