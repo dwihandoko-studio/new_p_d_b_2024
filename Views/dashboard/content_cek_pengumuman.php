b94f2b9013e8a723db26923472395f1e4f674da7
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-06-26 04:15:42 +0700

    update dashboard public

--- Views/dashboard/content_cek_pengumuman.php
+++ Views/dashboard/content_cek_pengumuman.php
@@ -18,22 +18,24 @@
             </span>
         </div>
     <?php } else { ?>
-        <div class="card text-white bg-primary">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item d-flex justify-content-between"><span class="mb-0">No Pendaftaran :</span><strong><?= $data->kode_pendaftaran ?></strong></li>
-                <li class="list-group-item d-flex justify-content-between"><span class="mb-0">Jalur :</span><strong><?= $data->via_jalur ?></strong></li>
-                <li class="list-group-item d-flex justify-content-between"><span class="mb-0">Nama Peserta :</span><strong><?= $data->nama_peserta ?></strong></li>
-                <li class="list-group-item d-flex justify-content-between"><span class="mb-0">NISN :</span><strong><?= $data->nisn_peserta ?></strong></li>
-                <li class="list-group-item d-flex justify-content-between"><span class="mb-0">Tanggal Lahir :</span><strong><?= $data->tanggal_lahir_peserta ?></strong></li>
-                <li class="list-group-item d-flex justify-content-between"><span class="mb-0">Sekolah Asal :</span><strong><?= $data->nama_sekolah_asal ?></strong></li>
-                <li class="list-group-item d-flex justify-content-between"><span class="mb-0">NPSN Asal :</span><strong><?= $data->npsn_sekolah_asal ?></strong></li>
-                <li class="list-group-item d-flex justify-content-between"><span class="mb-0">Sekolah Tujuan :</span><strong><?= $data->nama_sekolah_tujuan ?></strong></li>
-                <li class="list-group-item d-flex justify-content-between"><span class="mb-0">NPSN Tujuan :</span><strong><?= $data->npsn_sekolah_tujuan ?></strong></li>
-                <li class="list-group-item d-flex justify-content-between"><span class="mb-0">Jarak Domisili :</span><strong><?= round($data->jarak_domisili, 3) ?> Km</strong></li>
-            </ul>
-            <span>
-                <center>Anda dinyatakan <b>TIDAK LOLOS</b> seleksi PPDB Tahun Ajaran 2024/2025 <br />di : <b><?= $data->nama_sekolah_tujuan ?></b> Melalui Jalur <b><?= $data->via_jalur ?></b>.</center>
-            </span>
-        </div>
+        <?php if ((int)$data->status_pendaftaran === 3) { ?>
+            <div class="card text-white bg-primary">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item d-flex justify-content-between"><span class="mb-0">No Pendaftaran :</span><strong><?= $data->kode_pendaftaran ?></strong></li>
+                    <li class="list-group-item d-flex justify-content-between"><span class="mb-0">Jalur :</span><strong><?= $data->via_jalur ?></strong></li>
+                    <li class="list-group-item d-flex justify-content-between"><span class="mb-0">Nama Peserta :</span><strong><?= $data->nama_peserta ?></strong></li>
+                    <li class="list-group-item d-flex justify-content-between"><span class="mb-0">NISN :</span><strong><?= $data->nisn_peserta ?></strong></li>
+                    <li class="list-group-item d-flex justify-content-between"><span class="mb-0">Tanggal Lahir :</span><strong><?= $data->tanggal_lahir_peserta ?></strong></li>
+                    <li class="list-group-item d-flex justify-content-between"><span class="mb-0">Sekolah Asal :</span><strong><?= $data->nama_sekolah_asal ?></strong></li>
+                    <li class="list-group-item d-flex justify-content-between"><span class="mb-0">NPSN Asal :</span><strong><?= $data->npsn_sekolah_asal ?></strong></li>
+                    <li class="list-group-item d-flex justify-content-between"><span class="mb-0">Sekolah Tujuan :</span><strong><?= $data->nama_sekolah_tujuan ?></strong></li>
+                    <li class="list-group-item d-flex justify-content-between"><span class="mb-0">NPSN Tujuan :</span><strong><?= $data->npsn_sekolah_tujuan ?></strong></li>
+                    <li class="list-group-item d-flex justify-content-between"><span class="mb-0">Jarak Domisili :</span><strong><?= round($data->jarak_domisili, 3) ?> Km</strong></li>
+                </ul>
+                <span>
+                    <center>Anda dinyatakan <b>TIDAK LOLOS</b> seleksi PPDB Tahun Ajaran 2024/2025 <br />di : <b><?= $data->nama_sekolah_tujuan ?></b> Melalui Jalur <b><?= $data->via_jalur ?></b>.</center>
+                </span>
+            </div>
+        <?php } ?>
     <?php } ?>
 </div>
