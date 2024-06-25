1debd73f103520b85605dc6c06ea226b99422c42
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-06-25 22:20:11 +0700

    update dashboard public

--- Views/adm/pengaduan/detail.php
+++ Views/adm/pengaduan/detail.php
@@ -64,6 +64,9 @@
                                                     <button onclick="prosesPengaduan('<?= $data->no_tiket ?>', '<?= ucfirst(strtolower($data->nama_pengadu)) ?>')" class="btn btn-sm btn-primary">Proses Pengaduan</button>
                                                 <?php } else { ?>
                                                     <?php if ((int)$data->status == 3 || (int)$data->status == 2) { ?>
+                                                        <?php if ((int)$data->status == 2) { ?>
+                                                            <button onclick="sendPesan('<?= $data->nohp_pengadu ?>', '<?= $data->email_pengadu ?>', '<?= $data->no_tiket ?>', '<?= $data->nama_pengadu ?>', '', '<?= base_url('auth') . '/download?t=' . $data->no_tiket ?>')" class="btn btn-block btn-sm btn-warning">Kirim Ulang Notifikasi</button>
+                                                        <?php } ?>
                                                     <?php } else { ?>
                                                         <div class="row">
                                                             <div class="col-lg-4">
