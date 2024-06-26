badfe0809b126fff010089e2f78fc61d7b104a71
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-06-26 09:36:37 +0700

    update dashboard public

--- Controllers/Pd/Home.php
+++ Controllers/Pd/Home.php
@@ -223,7 +223,7 @@ class Home extends BaseController
                     ]
                 ],
                 '_file' => [
-                    'rules' => 'uploaded[_file]|max_size[_file,2048]|mime_in[_file,image/jpeg,image/jpg,image/png]',
+                    'rules' => 'uploaded[_file]|max_size[_file,5048]|mime_in[_file,image/jpeg,image/jpg,image/png]',
                     'errors' => [
                         'uploaded' => 'Pilih file terlebih dahulu. ',
                         'max_size' => 'Ukuran file terlalu besar, Maximum 2 Mb. ',
