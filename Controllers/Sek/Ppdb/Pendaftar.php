7dead633130e3edcccca4111e72ed6c870562583
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-06-27 21:53:22 +0700

    update dashboard public

--- Controllers/Sek/Ppdb/Pendaftar.php
+++ Controllers/Sek/Ppdb/Pendaftar.php
@@ -135,11 +135,13 @@ class Pendaftar extends BaseController
 
         if (!($oldData->id_perubahan == "" || $oldData->id_perubahan == null)) {
             $perubahanData = $this->_db->table('riwayat_perubahan_data a')
-                ->select("a.*, b.nama as nama_admin_perubahan")
-                ->join('_users_profile_sekolah b', 'a.user_id = b.user_id')
-                ->where("JSON_CONTAINS(a.data_lama, '$.kode_pendaftaran', '$oldData->kode_pendaftaran')")
-                ->orderBy('a.created_at', 'ASC')
+                ->select('a.*')
+                ->select('b.nama as nama_admin_perubahan')
+                ->join('_users_profile_sekolah b', 'a.user_id = b.user_id', 'left')
+                ->where("JSON_UNQUOTE(JSON_EXTRACT(a.data_lama, '$.kode_pendaftaran')) = '$oldData->kode_pendaftaran'")
+                ->orderBy('a.created_at', 'DESC')
                 ->get()->getResult();
+
             if (count($perubahanData) > 0) {
                 $data['riwayat_perubahan_data'] = $perubahanData;
             }
