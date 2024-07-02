d2f3a48cc9bc5278f963d14c56c925e34c305ed7
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-07-02 11:43:58 +0700

    update dashboard public

--- Controllers/Adm/Download.php
+++ Controllers/Adm/Download.php
@@ -999,6 +999,102 @@ class Download extends BaseController
         }
     }
 
+    public function dalamkab()
+    {
+        $Profilelib = new Profilelib();
+        $user = $Profilelib->user();
+        if ($user->status != 200) {
+            delete_cookie('jwt');
+            session()->destroy();
+            return redirect()->to(base_url('auth'));
+        }
+
+        try {
+
+            $spreadsheet = new Spreadsheet();
+            $worksheet = $spreadsheet->getActiveSheet();
+
+            $boldFont = new Font();
+            $boldFont->setBold(true);
+
+            // Menulis nama kolom ke dalam baris pertama worksheet
+            $worksheet->getCell('A1')->setValue("REKAPITULASI PESERTA PPDB LULUSAN SEKOLAH");
+            $worksheet->getCell('A2')->setValue("WILAYAH DALAM KABUPATEN LAMPUNG TENGAH");
+            $worksheet->getCell('A3')->setValue("TAHUN PELAJARAN 2024/2025");
+            $worksheet->mergeCells('A5:A6');
+            $worksheet->mergeCells('B5:B6');
+            $worksheet->mergeCells('C5:C6');
+            $worksheet->mergeCells('D5:D6');
+            $worksheet->mergeCells('E5:E6');
+            $worksheet->mergeCells('F5:F6');
+            $worksheet->mergeCells('G5:G6');
+            $worksheet->fromArray(['NO', 'KECAMATAN', 'NPSN', 'SATUAN PENDIDIKAN', 'JENJANG', 'STATUS', 'JUMLAH'], NULL, 'A5');
+
+            $worksheet->getColumnDimension('A')->setWidth(5);
+            $worksheet->getColumnDimension('B')->setWidth(30);
+            $worksheet->getColumnDimension('C')->setWidth(10);
+            $worksheet->getColumnDimension('D')->setWidth(50);
+            $worksheet->getColumnDimension('E')->setWidth(8);
+            $worksheet->getColumnDimension('F')->setWidth(7);
+            $worksheet->getColumnDimension('G')->setWidth(9);
+            // Mengambil data dari database
+            $query = $this->_db->table('_setting_kuota_tb a')
+                ->select("a.sekolah_id, b.kecamatan, a.npsn, b.nama, b.bentuk_pendidikan_id, b.bentuk_pendidikan, b.status_sekolah, (SELECT count(*) FROM _tb_pendaftar JOIN dapo_sekolah ON dapo_sekolah.sekolah_id = _tb_pendaftar.from_sekolah_id WHERE _tb_pendaftar.tujuan_sekolah_id_1 = a.sekolah_id AND dapo_sekolah.kode_kabupaten = '120200') AS jumlah_peserta")
+                ->join('dapo_sekolah b', 'a.sekolah_id = b.sekolah_id')
+                ->where("b.bentuk_pendidikan_id = 5")
+                ->orderBy('b.bentuk_pendidikan_id', 'DESC')
+                ->orderBy('b.status_sekolah', 'ASC')
+                ->orderBy('b.nama', 'ASC')
+                ->get();
+
+            // Menulis data ke dalam worksheet
+            $data = $query->getResult();
+            $row = 7;
+            if (count($data) > 0) {
+                foreach ($data as $key => $item) {
+                    $worksheet->getCell('A' . $row)->setValue($key + 1);
+                    $worksheet->getCell('B' . $row)->setValue($item->kecamatan);
+                    $worksheet->getCell('C' . $row)->setValue($item->npsn);
+                    $worksheet->getCell('D' . $row)->setValue($item->nama);
+                    $worksheet->getCell('E' . $row)->setValue($item->bentuk_pendidikan);
+                    $worksheet->getCell('F' . $row)->setValue($item->status_sekolah);
+                    $worksheet->getCell('G' . $row)->setValue($item->jumlah_peserta);
+                    $row++;
+                }
+            }
+
+
+            $styleKop = $worksheet->getStyle('A1:A4');
+            $styleKop->setFont($boldFont);
+
+            $styleHeader = $worksheet->getStyle('A5:G6');  // Adjust range based on your merged cells
+
+            // Set vertical and horizontal alignment
+            $styleHeader->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+            $styleHeader->getAlignment()->setVertical(Alignment::VERTICAL_CENTER);
+            // Apply bold font style to header cells
+            $styleHeader->setFont($boldFont);
+
+            $rowsToStyle = range('F', 'G');
+
+            foreach ($rowsToStyle as $row) {
+                $styleRow = $worksheet->getStyle($row);
+                $styleRow->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+                $styleRow->getAlignment()->setVertical(Alignment::VERTICAL_CENTER);
+            }
+
+            $writer = new Xls($spreadsheet);
+            $filename = 'DATA_REKAPITULASI_PESERTA_PPDB_LULUSAN_DALAM_WILAYAH_KABUPATEN.xls';
+            header('Content-Type: application/vnd-ms-excel');
+            header('Content-Disposition: attachment;filename="' . $filename . '"');
+            header('Cache-Control: max-age=0');
+            $writer->save('php://output');
+            exit();
+        } catch (\Throwable $th) {
+            var_dump($th);
+        }
+    }
+
     public function download()
     {
         if ($this->request->isAJAX()) {
--- Views/adm/download/index.php
+++ Views/adm/download/index.php
@@ -31,27 +31,35 @@
             </div>
             <div class="col-xl-4 mb-3">
                 <a href="<?= base_url('adm/download/afirmasi') ?>" target="_blank" class="btn btn-rounded btn-danger"><span class="btn-icon-start text-danger"><i class="fa fa-download color-danger"></i>
-                    </span>Download Rekapitulasi Jalur Afirmasi</a>
+                    </span>Download Rekap Jalur Afirmasi</a>
             </div>
             <div class="col-xl-4 mb-3">
                 <a href="<?= base_url('adm/download/zonasi') ?>" target="_blank" class="btn btn-rounded btn-secondary"><span class="btn-icon-start text-secondary"><i class="fa fa-download color-secondary"></i>
-                    </span>Download Rekapitulasi Jalur Zonasi</a>
+                    </span>Download Rekap Jalur Zonasi</a>
             </div>
             <div class="col-xl-4 mb-3">
                 <a href="<?= base_url('adm/download/mutasi') ?>" target="_blank" class="btn btn-rounded btn-light"><span class="btn-icon-start text-light"><i class="fa fa-download color-light"></i>
-                    </span>Download Rekapitulasi Jalur Mutasi</a>
+                    </span>Download Rekap Jalur Mutasi</a>
             </div>
             <div class="col-xl-4 mb-3">
                 <a href="<?= base_url('adm/download/prestasi') ?>" target="_blank" class="btn btn-rounded btn-primary"><span class="btn-icon-start text-primary"><i class="fa fa-download color-primary"></i>
-                    </span>Download Rekapitulasi Jalur Prestasi</a>
+                    </span>Download Rekap Jalur Prestasi</a>
             </div>
             <div class="col-xl-4 mb-3">
                 <a href="<?= base_url('adm/download/swasta') ?>" target="_blank" class="btn btn-rounded btn-info"><span class="btn-icon-start text-info"><i class="fa fa-download color-info"></i>
-                    </span>Download Rekapitulasi Sekolah Swasta</a>
+                    </span>Download Rekap Sekolah Swasta</a>
             </div>
             <div class="col-xl-4 mb-3">
                 <a href="<?= base_url('adm/download/belumsekolah') ?>" target="_blank" class="btn btn-rounded btn-warning"><span class="btn-icon-start text-warning"><i class="fa fa-download color-warning"></i>
-                    </span>Download Rekapitulasi Belum Sekolah</a>
+                    </span>Download Rekap Belum Sekolah</a>
+            </div>
+            <div class="col-xl-4 mb-3">
+                <a href="<?= base_url('adm/download/dalamkab') ?>" target="_blank" class="btn btn-rounded btn-danger"><span class="btn-icon-start text-danger"><i class="fa fa-download color-danger"></i>
+                    </span>Download Rekap Dalam Wilayah</a>
+            </div>
+            <div class="col-xl-4 mb-3">
+                <a href="<?= base_url('adm/download/luarkab') ?>" target="_blank" class="btn btn-rounded btn-secondary"><span class="btn-icon-start text-secondary"><i class="fa fa-download color-secondary"></i>
+                    </span>Download Rekap Luar Wilayah</a>
             </div>
         </div>
         <!-- </div> -->
