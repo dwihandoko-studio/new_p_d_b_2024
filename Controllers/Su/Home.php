6d1415650cb861426dc56cc94fe60d76c1e2fcf7
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-07-09 21:15:34 +0700

    add test token

--- Controllers/Su/Home.php
+++ Controllers/Su/Home.php
@@ -397,8 +397,8 @@ class Home extends BaseController
         curl_setopt($curlHandle, CURLOPT_CUSTOMREQUEST, "GET");
         // curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $data);
         curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-        $username = 'ifkhar.halim@lampungtengahkab.go.id';
-        $password = 'HalimGolok@123#';
+        $username = '';
+        $password = '';
         $authorization = base64_encode($username . ':' . $password); // Encode username:password
 
         curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array(
