d8a8d6f7b0f504c4d3d9ad18ddb77cc52dfa392f
dwihandoko-studio <dwi.handoko0511@gmail.com>
2024-07-11 23:21:01 +0700

    add email

--- Controllers/Su/Home.php
+++ Controllers/Su/Home.php
@@ -474,22 +474,10 @@ class Home extends BaseController
         }
         echo $tokenSyn->token;
         echo "<br/>";
-        $datas = $this->_db->table('data_balikan_via_api')->where("cant_sync = 0")->where("id IN (SELECT id FROM aa_gagal_syn_balikan WHERE keterangan = 'Data terdeteksi sudah ada sebelumnya.')")->limit(50)->get()->getResult();
-        // $subQuery = $this->_db->table('aa_gagal_syn_balikan')
-        //     ->select('id')
-        //     ->where('keterangan', 'Data terdeteksi sudah ada sebelumnya.')
-        //     ->limit(50);
-
-        // $datas = $this->_db->table('data_balikan_via_api')
-        //     ->where('status_syn', 0)
-        //     ->where('cant_sync', 0)
-        //     ->whereIn('id', $subQuery)
-        //     ->limit(50)
-        //     ->get()
-        //     ->getResult();
+        $datas = $this->_db->table('data_balikan_via_api_tk')->where("status_syn = 0 AND cant_sync = 0")->limit(50)->get()->getResult();
         if (count($datas) > 0) {
             foreach ($datas as $key => $value) {
-                $this->_db->table('data_balikan_via_api')->where('id', $value->id)->update([
+                $this->_db->table('data_balikan_via_api_tk')->where('id', $value->id)->update([
                     'has_syn' => 1,
                 ]);
                 $result = $this->sendDataBalikan($value, $tokenSyn->token);
@@ -596,14 +584,14 @@ class Home extends BaseController
                     return $sukses;
                 } else {
                     if ((int)$result->statusCode == 203) {
-                        $this->deleteGagalSyn($data->id);
+                        $this->insertGagalSyn($data->id, $data, $result->message);
                         $sukses = [
                             'status_sync' => 1,
                             'message' => $result->message
                         ];
                         return $sukses;
                     } else {
-                        $this->insertGagalSyn($data->id, $data, $result->message);
+                        $this->deleteGagalSyn($data->id);
                         $sukses = [
                             'status_sync' => 1,
                             'message' => $result->message
@@ -664,6 +652,173 @@ class Home extends BaseController
             'updated_syn' => date('Y-m-d H:i:s')
         ];
 
-        return $this->_db->table('data_balikan_via_api')->where('id', $id)->update($data_update);
+        return $this->_db->table('data_balikan_via_api_tk')->where('id', $id)->update($data_update);
     }
+
+
+    // public function synDataBalikan()
+    // {
+    //     // set_time_limit(0);
+
+    //     ob_start();
+    //     $tokenSyn = $this->_db->table('aa_token_sync')->where('id', 1)->get()->getRowObject();
+    //     if (!$tokenSyn) {
+    //         echo "TOKEN SYNCRONE TIDAK ADA.";
+    //         die;
+    //     }
+    //     echo $tokenSyn->token;
+    //     echo "<br/>";
+    //     $datas = $this->_db->table('data_balikan_via_api')->where("cant_sync = 0")->where("id IN (SELECT id FROM aa_gagal_syn_balikan WHERE keterangan = 'Data terdeteksi sudah ada sebelumnya.')")->limit(50)->get()->getResult();
+    //     // $subQuery = $this->_db->table('aa_gagal_syn_balikan')
+    //     //     ->select('id')
+    //     //     ->where('keterangan', 'Data terdeteksi sudah ada sebelumnya.')
+    //     //     ->limit(50);
+
+    //     // $datas = $this->_db->table('data_balikan_via_api')
+    //     //     ->where('status_syn', 0)
+    //     //     ->where('cant_sync', 0)
+    //     //     ->whereIn('id', $subQuery)
+    //     //     ->limit(50)
+    //     //     ->get()
+    //     //     ->getResult();
+    //     if (count($datas) > 0) {
+    //         foreach ($datas as $key => $value) {
+    //             $this->_db->table('data_balikan_via_api')->where('id', $value->id)->update([
+    //                 'has_syn' => 1,
+    //             ]);
+    //             $result = $this->sendDataBalikan($value, $tokenSyn->token);
+    //             if (isset($result['status_sync'])) {
+    //                 if ($result['status_sync'] == 1) {
+    //                     echo "Berhasil Sync: $value->id<br/>";
+    //                     var_dump($result['message']);
+    //                     echo "<br/>";
+    //                     ob_flush();
+    //                     flush();
+    //                     continue;
+    //                 } else {
+    //                     echo "Gagal Sync: $value->id<br/>";
+    //                     var_dump($result['error']);
+    //                     ob_flush();
+    //                     flush();
+    //                     die;
+    //                 }
+    //             } else {
+    //                 echo "Gagal Sync: $value->id<br/>";
+    //                 ob_flush();
+    //                 flush();
+    //                 die;
+    //             }
+    //         }
+    //     } else {
+    //         echo "TIDAK ADA DATA.";
+    //         die;
+    //     }
+    // }
+
+    // private function sendDataBalikan($data, $tokenBearer)
+    // {
+    //     $data_peserta = [
+    //         'token' => getenv('lay.default.token'),
+    //         'peserta_didik_id' => $data->peserta_didik_id ?? NULL,
+    //         'sekolah_id_asal' => $data->sekolah_id_asal ?? NULL,
+    //         'npsn_sekolah_asal' => $data->npsn_sekolah_asal ?? NULL,
+    //         'nama_sekolah_asal' => $data->nama_sekolah_asal ?? NULL,
+    //         'nik' => $data->nik ?? NULL,
+    //         'nisn' => $data->nisn ?? NULL,
+    //         'nama' => $data->nama ?? NULL,
+    //         'tempat_lahir' => $data->tempat_lahir ?? NULL,
+    //         'tanggal_lahir' => $data->tanggal_lahir ?? NULL,
+    //         'jenis_kelamin' => $data->jenis_kelamin ?? NULL,
+    //         'nik_ibu' => $data->nik_ibu ?? NULL,
+    //         'nama_ibu_kandung' => $data->nama_ibu_kandung ?? NULL,
+    //         'nama_ayah' => $data->nama_ayah ?? NULL,
+    //         'nik_ayah' => $data->nik_ayah ?? NULL,
+    //         'nama_wali' => $data->nama_wali ?? NULL,
+    //         'nik_wali' => $data->nik_wali ?? NULL,
+    //         'alamat_jalan' => $data->alamat_jalan ?? NULL,
+    //         'rt' => $data->rt ?? NULL,
+    //         'rw' => $data->rw ?? NULL,
+    //         'nama_dusun' => $data->nama_dusun ?? NULL,
+    //         'desa_kelurahan' => $data->desa_kelurahan ?? NULL,
+    //         'kode_wilayah_siswa' => $data->kode_wilayah_siswa ?? NULL,
+    //         'lintang' => $data->lintang ?? NULL,
+    //         'bujur' => $data->bujur ?? NULL,
+    //         'kebutuhan_khusus_id' => $data->kebutuhan_khusus_id ?? NULL,
+    //         'agama_id' => $data->agama_id ?? NULL,
+    //         'no_kk' => $data->no_kk ?? NULL,
+    //         'sekolah_id_tujuan' => $data->sekolah_id_tujuan ?? NULL,
+    //         'npsn_sekolah_tujuan' => $data->npsn_sekolah_tujuan ?? NULL,
+    //         'nama_sekolah_tujuan' => $data->nama_sekolah_tujuan ?? NULL
+    //     ];
+
+
+    //     // $tokenBearer = getenv('lay.default.token_bearer');
+
+    //     $curlHandle = curl_init("http://118.98.237.214/v1/api-gateway/pd/tambahDataHasilPPDB");
+    //     curl_setopt($curlHandle, CURLOPT_CUSTOMREQUEST, "POST");
+    //     curl_setopt($curlHandle, CURLOPT_POSTFIELDS, json_encode($data_peserta));
+    //     curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+    //     curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array(
+    //         'Authorization: Bearer ' . $tokenBearer,
+    //         'Content-Type: application/json'
+    //     ));
+    //     curl_setopt($curlHandle, CURLOPT_TIMEOUT, 30);
+    //     curl_setopt($curlHandle, CURLOPT_CONNECTTIMEOUT, 30);
+
+    //     $send_data         = curl_exec($curlHandle);
+
+    //     $result = json_decode($send_data);
+
+    //     if (isset($result->error)) {
+    //         $gagal = [
+    //             'status_sync' => 0,
+    //             'error' => $result->error
+    //         ];
+    //         return $gagal;
+    //     }
+
+    //     if ($result) {
+
+    //         if (isset($result->statusCode)) {
+    //             if ((int)$result->statusCode == 200) {
+    //                 $this->updateBerhasilSyn($data->id);
+    //                 $this->deleteGagalSyn($data->id);
+    //                 $sukses = [
+    //                     'status_sync' => 1,
+    //                     'message' => $result->message
+    //                 ];
+    //                 return $sukses;
+    //             } else {
+    //                 if ((int)$result->statusCode == 203) {
+    //                     $this->deleteGagalSyn($data->id);
+    //                     $sukses = [
+    //                         'status_sync' => 1,
+    //                         'message' => $result->message
+    //                     ];
+    //                     return $sukses;
+    //                 } else {
+    //                     $this->insertGagalSyn($data->id, $data, $result->message);
+    //                     $sukses = [
+    //                         'status_sync' => 1,
+    //                         'message' => $result->message
+    //                     ];
+    //                     return $sukses;
+    //                 }
+    //             }
+    //         } else {
+    //             $gagal = [
+    //                 'status_sync' => 0,
+    //                 'error' => $result
+    //             ];
+    //             return $gagal;
+    //         }
+    //     } else {
+    //         $gagal = [
+    //             'status_sync' => 0,
+    //             'error' => $send_data
+    //         ];
+    //         return $gagal;
+    //     }
+    // }
+
 }
